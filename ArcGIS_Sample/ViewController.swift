48d84cc637b3586280e50f58c3a7bed6c56b8f3b
Ryoichi Tanaka <ryoichitanakajp@gmail.com>
2016-08-10 15:23:16 +0900

    Initial Commit

---
+++ ArcGIS_Sample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ArcGIS_Sample
+//
+//  Created by tnk on 2016/08/10.
+//  Copyright © 2016年 goga.co.jp. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
