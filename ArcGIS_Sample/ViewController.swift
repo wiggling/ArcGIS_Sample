18f6fcc28208b3e43fb3335ab1f404459fc727bf
Ryoichi Tanaka <ryoichitanakajp@gmail.com>
2016-08-10 15:49:59 +0900

    WebMap表示機能を実装

--- ArcGIS_Sample/ViewController.swift
+++ ArcGIS_Sample/ViewController.swift
@@ -7,19 +7,46 @@
 //
 
 import UIKit
+import ArcGIS
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, AGSMapViewLayerDelegate {
+
+	@IBOutlet weak var mapView: AGSMapView!
+	var webMap: AGSWebMap!
 
 	override func viewDidLoad() {
 		super.viewDidLoad()
-		// Do any additional setup after loading the view, typically from a nib.
+
+		// ArcGIS Runtime Basicライセンス認証
+		let clientID = "<my_client_ID>"
+		do {
+			try AGSRuntimeEnvironment.setClientID(clientID)
+		}
+		catch {
+			print("setClientID failed: \(error)")
+		}
+
+		// Webマップの取得
+		self.webMap = AGSWebMap(itemId: "c59da60680024f2f87f444c7ddc82fb2", credential: nil)
+
+		// AGSMapViewでWebマップを開く
+		self.webMap.openIntoMapView(self.mapView)
+
+		// AGSMapViewのデリゲート設定
+		self.mapView.layerDelegate = self
 	}
 
-	override func didReceiveMemoryWarning() {
-		super.didReceiveMemoryWarning()
-		// Dispose of any resources that can be recreated.
+	// AGSMapViewのデリゲートメソッド（地図ロード完了時に実行される）
+	func mapViewDidLoad(mapView: AGSMapView!) {
+
+		// 位置情報の表示モードを設定
+		self.mapView.locationDisplay.autoPanMode = .Default
+
+		// 地図が現在位置にズームされる際の表示縮尺の設定
+		self.mapView.locationDisplay.zoomScale = 100000
+
+		// 現在位置の表示を開始
+		self.mapView.locationDisplay.startDataSource()
 	}
-
-
 }
 
